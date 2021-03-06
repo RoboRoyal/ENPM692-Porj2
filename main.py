7103dad552dc072ed17a8b2d0d9773ae01625567
RoboRoyal <abernathy900@umbc.edu>
2021-03-05 22:52:48 -0500

    Displa(board) working with map drawn in

--- main.py
+++ main.py
@@ -1,6 +1,59 @@
 
 #Dakota Abernathy
 #ENPM692-Porj2
+import pygame
+import time
+
+HEIGHT = 300
+WIDTH = 400
+SCALE = 2
+
+display = None
+start = None
+target = None
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+CYAN = (0, 255, 255)
+MAGENTA = (255, 0, 255)
+YELLOW = (255, 255, 0)
+
+def makeBoard():
+    global display
+    pygame.init()
+    display = pygame.display.set_mode((int(WIDTH * SCALE), int(HEIGHT * SCALE)))
+    pygame.display.set_caption("Path finding algorithm")
+    display.fill(WHITE)
+
+    #
+    pygame.draw.circle(display, BLACK, [90 * SCALE, (HEIGHT - 70) * SCALE], 35 * SCALE)
+    pygame.draw.ellipse(display, BLACK, [175 * SCALE, (HEIGHT - 175) * SCALE, 120 * SCALE, 60 * SCALE], 0 * SCALE)
+
+    # Line Segment
+    pygame.draw.polygon(display, BLACK,
+                        [(48 * SCALE, (HEIGHT - 108) * SCALE), (37 * SCALE, (HEIGHT - 124) * SCALE),
+                         (159 * SCALE, (HEIGHT - 210) * SCALE), (170 * SCALE, (HEIGHT - 194) * SCALE), ])
+
+    # C shape
+    pygame.draw.polygon(display, BLACK,  # back
+                        [(200 * SCALE, (HEIGHT - 270) * SCALE), (210 * SCALE, (HEIGHT - 270) * SCALE),
+                         (210 * SCALE, (HEIGHT - 240) * SCALE), (200 * SCALE, (HEIGHT - 240) * SCALE)])
+    pygame.draw.polygon(display, BLACK,  # top
+                        [(200 * SCALE, (HEIGHT - 280) * SCALE), (230 * SCALE, (HEIGHT - 280) * SCALE),
+                         (230 * SCALE, (HEIGHT - 270) * SCALE), (200 * SCALE, (HEIGHT - 270) * SCALE)])
+    pygame.draw.polygon(display, BLACK,  # bottom
+                        [(200 * SCALE, (HEIGHT - 240) * SCALE), (230 * SCALE, (HEIGHT - 240) * SCALE),
+                         (230 * SCALE, (HEIGHT - 230) * SCALE), (200 * SCALE, (HEIGHT - 230) * SCALE)])
+
+    # Polygon ---- whats the error allowed? lot of rounding and re-rounding
+    pygame.draw.polygon(display, BLACK,  # why is this so ugly
+                        [(354 * SCALE, (HEIGHT - 138) * SCALE), (380 * SCALE, (HEIGHT - 170) * SCALE),
+                         (380 * SCALE, (HEIGHT - 115) * SCALE), (328 * SCALE, (HEIGHT - 63) * SCALE),
+                         (286 * SCALE, (HEIGHT - 105) * SCALE), (325 * SCALE, (HEIGHT - 143) * SCALE)])
 
 if __name__ == "__main__":
-    print("Hello world")
+    makeBoard()
+    pygame.display.update()
+    time.sleep(5)
